0cf4d05e529936667068b7f9a690c5b1001dff56
Crispymug <139268379+Crispymug@users.noreply.github.com>
2023-09-18 09:15:07 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,6 +7,13 @@ radio.onReceivedString(function (receivedString) {
     for (let index = 0; index < 4; index++) {
         radio.sendString(receivedString)
     }
+    basic.showLeds(`
+        # . . . #
+        # # . # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
     if (data != receivedString) {
         if (receivedString.split(".")[0] == "m") {
             serial.writeLine("" + (receivedString.split(".")[1]))
@@ -28,6 +35,13 @@ input.onButtonPressed(Button.B, function () {
     serial.writeLine("IP:" + ip)
 })
 serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        # # . # #
+        # . . . #
+        `)
     data = serial.readLine()
     serial.writeString(serial.readLine())
     for (let index = 0; index < 5; index++) {
